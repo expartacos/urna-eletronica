6400d7ca30bf073b85f55cacec5baf56e7d916f6
Rafael Pereira <rafaelpersan@gmail.com>
2023-06-21 10:16:49 -0300

    Mudança na tela da urna e adicionando o arquivo json = etapas.js

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        tilulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 12345,
+                nome: 'Rafael',
+                partido: 'QUALQUER',
+                fotos: [
+                    {url: '38111.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                numero: 54321,
+                nome: 'Ari',
+                partido: 'QUALQUER',
+                fotos: [
+                    {url: '77222.jpg', legenda: 'Vereador', small: false}
+                ]
+            }
+        ]
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 13,
+                nome: 'Max',
+                partido: 'PT',
+                vice: 'Bere',
+                fotos: [
+                    {url: '84.jpg', legenda: 'Prefeito', small: false},
+                    {url: '84.jpg', legenda: 'Vice-Prefeito', small: true}
+                ] 
+            },
+            {
+                numero: 45,
+                nome: 'Diu',
+                partido: 'esquerda',
+                vice: 'Lurdes',
+                fotos: [
+                    {url: '99.jpg', legenda: 'Prefeito', small: false},
+                    {ùrl: '99-2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            }
+        ]
+    }
+];
